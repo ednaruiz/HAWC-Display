2ebffadba41511e7bf41ef34e90390dabed24a50
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-05-08 13:02:16 -0500

    Update display-w-colors.py

--- Display/display-w-colors.py
+++ Display/display-w-colors.py
@@ -87,104 +87,110 @@ def FindEntry(tinit,tfin):
 
 
 def PlayDisplayWGraph( tinit, tfin ):
-
-    pastT  = np.zeros(1200)
-    pastCh = np.zeros(1200)
-
+    
     C = "gray"
-
     steps = 10
-    chinit,chfin = FindEntry(tinit,tfin)
-    
-    for jentry in np.arange(chinit+steps,chfin+steps,steps):
-        fig ,ax = plt.subplots(num=None,  figsize=(12, 5), dpi=80, facecolor='w', edgecolor='k')
-        plt.suptitle("HAWC Display")
-        plt.subplot(121)
-        plt.xlabel("Survey x [m]")
-        plt.ylabel("Survey y [m]")
-        plt.xlim(-90,160)
-        plt.ylim(150,345)
-        
- 
-        # get the next tree in the chain and verify
-        ientry = mychain.LoadTree( jentry )
-        if ientry < 0:
-            break
-        
-        # copy next entry into memory and verify
-        nb = mychain.GetEntry( jentry )
-        if nb <= 0:
-            continue
-        
 
-        ChargeT =  np.zeros(300)
+    chinit, chfin = FindEntry(tinit,tfin)
+    
+    ChargeP =  np.zeros(1200)
+    TimeP = np.zeros(1200)
+    
+    j = 1.
+    tstep = tinit + j*steps
+    cont = 0
+    
+    CHARGE = []
+    TIME = []
+    TIME.append(TimeP)
+    for iCh in np.arange(chinit,chfin+2,1):
+      ientry = mychain.LoadTree( iCh )
+      if ientry < 0:
+        break
+        
+      # copy next entry into memory and verify
+      nb = mychain.GetEntry( iCh )
+      if nb <= 0:
+        continue
+            
+      
+      mychain.GetEntry(iCh)
+        
+      ichannel = mychain.Channel
+      icharge  = mychain.CalibratedCharge
+      itime = int(mychain.CalibratedTime)
+      
+      if (itime<tstep):
+        ChargeP[ichannel]=icharge
+        TimeP[ichannel]=itime
+        #print itime,tstep,j
+      else:
+        CHARGE.append(ChargeP)
+        TIME.append(TimeP)
+        j=j+1
+        tstep = tinit + j*steps
+        iCh = iCh - 1
+        cont = 0
         ChargeP =  np.zeros(1200)
-        TimeP =  np.zeros(1200)
-        ChannelP = np.zeros(1200)
-        while ( mychain.GetEntry(i) and mychain.CalibratedTime<jentry):
-            mychain.GetEntry(i)
-            ichannel = mychain.Channel
-            icharge  = mychain.CalibratedCharge
-            itime = mychain.CalibratedTime
-            ChargeT[int(floor((ichannel-1)/4))] = ChargeT[int(floor((ichannel-1)/4))] + icharge
-            ChargeP[ichannel-1] = ChargeP[ichannel-1] + icharge
-            TimeP[ichannel-1] = itime
-            ChannelP[ichannel-1] = ichannel
-            i = i+1
+    TIME.append(np.zeros(1200))
     
-        #print ChargeT
-        plt.scatter(Tsurvey[:,0]*0.01,Tsurvey[:,1]*0.01,s = 100, c = [ log(ich) for ich in ChargeT],cmap=plt.cm.jet , vmin=1, vmax = 10 , alpha=0.5)
-        plt.scatter(Psurvey[:,0]*0.01,Psurvey[:,1]*0.01,s = 10, c = [ log(ich) for ich in ChargeP],cmap=plt.cm.jet , vmin=1, vmax = 10 , alpha=0.5)
-        cbar = plt.colorbar()
-        cbar.set_label('log(Charge) [PE]')
+    for iEvent in range (0,len(CHARGE)):
+      ChargeT = np.zeros(300)
+      
+      for i in range(0,1200):
+        ChargeT[int(floor((i)/4))] = ChargeT[int(floor((i)/4))] + CHARGE[iEvent][i]
 
-        plt.scatter(Tsurvey[:,0]*0.01,Tsurvey[:,1]*0.01,s = 100,color=C,alpha=0.2)
-        plt.scatter(Psurvey[:,0]*0.01,Psurvey[:,1]*0.01,s = 10,color=C,alpha=0.2)
+      fig, ax = plt.subplots(num=None,  figsize=(12, 5), dpi=80, facecolor='w', edgecolor='k')
+      plt.title("HAWC Display")
+      plt.xlabel("Survey x [m]")
+      plt.ylabel("Survey y [m]")
+      plt.xlim(-90,160)
+      plt.ylim(150,345)
+      
+      t0 = tinit + iEvent*steps
+      tf = tinit + (iEvent+1)*steps
+      
+      plt.scatter(Tsurvey[:,0]*0.01,Tsurvey[:,1]*0.01,s = 100,color=C,alpha=0.2)
+      plt.scatter(Psurvey[:,0]*0.01,Psurvey[:,1]*0.01,s = 10,color=C,alpha=0.2)
+      plt.scatter(Tsurvey[:,0]*0.01,Tsurvey[:,1]*0.01,s = 100, c = [ log(ich) for ich in ChargeT ],cmap=plt.cm.jet , vmin=1, vmax = 10 , alpha=0.5)
+      plt.scatter(Psurvey[:,0]*0.01,Psurvey[:,1]*0.01,s = 10, c = [ log(ich) for ich in CHARGE[iEvent] ],cmap=plt.cm.jet , vmin=1, vmax = 10 , alpha=0.5)
+      cbar = plt.colorbar()
+      cbar.set_label('log(Charge) [PE]')
+      plt.text(-85,330,"Time: %i - %i ns"%(t0,tf),fontsize=10)
 
-        plt.text(-85,330,"Time: %i - %i ns"%(jentry-steps,jentry),fontsize=10)
+      plt.subplots_adjust( right=0.9)
+      
+      plt.subplot(222)
 
-        plt.subplots_adjust( right=0.9)
+      n , bins , patches = P.hist([round(log(ich),2) for ich in CHARGE[iEvent] if ich != 0],30, histtype='stepfilled')
+
+      P.setp(patches, 'facecolor', 'g', 'alpha', 0.35)
+      plt.ylim(-0.01,10.01)
+      plt.xlim(-1.01,10.01)
+      plt.xlabel("log(Charge) [PE]")
+      plt.ylabel("Number of events")
+
+      plt.subplot(224)
         
+      plt.scatter(TIME[iEvent],TIME[iEvent],s = 5,color=C,alpha=0.3)
+      plt.scatter(TIME[iEvent+1],TIME[iEvent+1],s = 5,color=C,alpha=0.9)      
+      plt.scatter(TIME[iEvent+2],TIME[iEvent+2],s = 5,color=C,alpha=0.3)
+      plt.axhline(y=t0-steps, color='r', linestyle='-')
+      plt.axhline(y=tf, color='r', linestyle='-')
+      ax = plt.gca()
+      ax.ticklabel_format(useOffset=False)
+      plt.ylim(t0-(steps*2),tf+(steps*2))
+      plt.xlim(0.001,1200.001)
+      plt.xlabel("PMT Channel")
+      plt.ylabel("Time [ns]")
+      plt.tight_layout()
         
-        
-        
-        plt.subplot(222)
+      fig.subplots_adjust(top=0.88)
 
-        n , bins , patches = P.hist([round(log(ich),2) for ich in ChargeP if ich != 0],30, histtype='stepfilled')
-
-        P.setp(patches, 'facecolor', 'g', 'alpha', 0.35)
-        plt.ylim(-0.01,10.01)
-        plt.xlim(-1.01,10.01)
-        plt.xlabel("log(Charge) [PE]")
-        plt.ylabel("Number of events")
-
-        plt.subplot(224)
-        
-        plt.scatter(pastCh,pastT,s = 5,color=C,alpha=0.3)
-        plt.scatter(ChannelP,TimeP,s = 5,color=C,alpha=0.9)
-        plt.axhline(y=jentry-steps, color='r', linestyle='-')
-        plt.axhline(y=jentry, color='r', linestyle='-')
-        ax = plt.gca()
-        ax.ticklabel_format(useOffset=False)
-        plt.ylim(jentry-(steps*2),jentry)
-        plt.xlim(0.001,1200.001)
-        plt.xlabel("PMT Channel")
-        plt.ylabel("Time [ns]")
-        
-        plt.tight_layout()
-        
-        fig.subplots_adjust(top=0.88)
-        
-        
-        plt.savefig("%i.png"%(jentry))
-        print "Image %i.png generated"%(jentry)
-
-        plt.close()
-
-        pastCh = ChannelP
-        pastT = TimeP
-
-
+      plt.savefig("%i.png"%(iEvent))
+      print "Image %i.png generated"%(iEvent)
+      plt.close()
+      
 
 def PlayDisplay( tinit, tfin ):
     C = "gray"
@@ -273,7 +279,7 @@ def main():
         print "Finding entry"
         FindEntry(start , final )
         PlayDisplay( start , final )
-        #GenGif(start , final )
+        GenGif(start , final )
 
     else:
         PlayDisplayWGraph( start , final )
